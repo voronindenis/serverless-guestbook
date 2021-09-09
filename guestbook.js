d388839675cb8eb1980a70987610b3153b2d9828
Denis Voronin <[voroninds@msk.bcs.ru]>
2021-09-09 15:30:40 +0300

    added apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://221436b2.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
